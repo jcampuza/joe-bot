318fbd107c8e283c6e6c9324fa996f5f5254ce58
Campuzano Joseph <campuzanojoseph@ppi-z9473lvdl.home>
2020-11-09 22:37:05 -0600

    add weather

---
+++ src/commands/weather.js
@@ -0,0 +1,43 @@
+const discord = require('discord.js');
+const axios = require('axios');
+
+const client = axios.create({
+  baseURL: 'https://api.openweathermap.org/data/2.5',
+  params: {
+    appid: process.env.OPEN_WEATHER_API_KEY,
+    units: 'imperial',
+  },
+});
+
+module.exports = {
+  name: 'weather',
+  description: 'Fetch the current weather',
+  /**
+   *
+   * @param {discord.Message} msg
+   */
+  execute: async (msg, args) => {
+    const q = args.join(' ');
+
+    try {
+      const { data } = await client.get('/weather', {
+        params: {
+          q,
+        },
+      });
+
+      console.log(data);
+
+      const title = `Current weather for ${data.name}`;
+      const type = `${data.weather[0].main}: ${data.weather[0].description}`;
+      const temp = `Temp: ${data.main.temp}, Feels Like: ${data.main.feels_like}`;
+      const wind = `Wind: ${data.wind.speed}`;
+
+      const response = [title, type, temp, wind].join('\n');
+
+      return msg.reply(response);
+    } catch (err) {
+      return msg.reply(`Oops, there was a problem fetching weather for ${q}`);
+    }
+  },
+};
